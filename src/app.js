3bf5a61d90bdf62249be19bf79de1504cd10e5e3
Ahamad <ahamad.jedu@gmail.com>
2017-05-07 21:14:45 +0700

    55. git push -u origin master

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import React,{Component} from "react";
+import {View,Text} from "react-native";
+
+
+
+class App extends Component{
+
+    render(){
+        return(
+            <View>
+                <Text>
+                    Hello Android ,Ios
+                </Text>
+            </View>
+            
+        );             
+    }
+
+
+}
+
+export default App;
